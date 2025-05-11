9f4f170c04727216fb5d587fa93bb68528a1c458
CHRISTIAN MOURA DOS SANTOS <cmourasiga@gmail.com>
2025-05-11 01:13:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 const VERIFY_TOKEN = "meu_token_webhook";
-const token = "EAAKOELSWQlIBO37ZA1NybZAMMl6tbwHTLBYONoqFa5UZAZBqfxZB5FZBjmXtUypHCAg8v1UrGFZAprmqQ81rBcyPbi3RU4EZCzxHk7WPkStbMPPZAhUDNUVelVAnpZAF1j0NuVwxeS020ASHtYYEIznsdTzke1lFApbTR3s10HRgYIGhVP7HoDvxjZCm9dsRhLNW5IC0AhyQI3uaisAgFJvpmcROGPWku4ZD";
+const token = "EAAKOELSWQlIBO4ZCyZCQQmLUqKX6PIGkgY0m2Lq5BY0XZBpNQRZBndfWZC6Af4ZA4cfYBDIbBUotiT3lQK2WaxIHzAfCpmXsecxAyOaa18Qnae3nYa5OO3VujYBkuhC0BFHVqokZA1rDGJszCoWuO8AAGCQTnub0QoCjimuzMGNYHM8aPoEacD2fxk6jMCQNhCr8QLCYSQYGNnUTLz27jDrt1Ox2pyb";
 const phone_number_id = "572870979253681";
 const makeWebhookURL = "https://hook.us2.make.com/la3lng90eob57s6gg6yg12s8rlmqy3eh";
 
