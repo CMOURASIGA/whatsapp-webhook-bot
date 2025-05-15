9c3c6fe4b4cfd4cf25a81560091c536fe4ef9274
CHRISTIAN MOURA DOS SANTOS <cmourasiga@gmail.com>
2025-05-15 14:18:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 const VERIFY_TOKEN = "meu_token_webhook";
-const token = "EAAKOELSWQlIBOy64i5ZCZAEy3HYPNvJwKVikZBbYNrZCX3Fx8Pw4HAZBHfkl1gGoH2qCzrBhSxTKAzlJFilDmbFp3h5ZBKjeAwPC16kaFsQWejxL8gHZAUZBxx3pYUrFR5wnj9mhS9a7b7kL47Qo5HUABpDs6mPlLMBZBYJZClldNnoe5etXsyydsdwYJV7wcRD1wVCreUQa0USgUmzmDPZCBnlX1IZD";
+const token = "EAAKOELSWQlIBO6gbFEaI4TdTDTtIbH5RcHQhdtZBW4yb0rHYkEZAfPBMRA9CsnwE0S3e7H9BKlpGEqfuTSaZBgrDb0kAw9ZBSPjVzLnZBxZC9CX8EUQBY7myZBAgLbt7FBhNmhHWNcQ1cCzeu89ZBy8FgcqjxEu74KC9tD8bFofWhEaUyL60NVkD2fvtSGTzhQMJqmeup4zZArZAtRogjcxnJVA3Or";
 const phone_number_id = "572870979253681";
 const makeWebhookMenu1 = "https://hook.us2.make.com/4avmjbxepfl59g3d7jbl8ovylik4mcm8";
 const makeWebhookMenu6 = "https://hook.us2.make.com/la3lng90eob57s6gg6yg12s8rlmqy3eh";
