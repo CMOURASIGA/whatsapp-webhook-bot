99dd178f763d3e03a85b927e214134a4b8deb392
Christian Moura dos Santos <christian_7c@cnc.org.br>
2025-05-21 11:51:14 -0300

    atualização item 6 link

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ app.post("/webhook", async (req, res) => {
       const saudacao = "📅 *Agenda de Eventos do EAC - Mês Atual*";
 
       try {
-        const resposta = await axios.get("https://script.google.com/macros/s/AKfycbyKiRCN2ynBvdkWqvNY-WjaNQ1_xriYRI-fh0QuX_Dd2fBZqRBCdyl1RizBbU4_mnOrbA/exec");
+        const resposta = await axios.get("https://script.google.com/macros/s/AKfycbwsVlwcpHMQzA28FS-gAKUL-GXS_8_7_kBJt80c2P8rxflo3RNfrwZzJYIodCbrjpOokQ/exec");
         const { status, links } = resposta.data;
 
         if (status === "SEM_EVENTOS") {
