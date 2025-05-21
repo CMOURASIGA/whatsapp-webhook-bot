a85339c34d9e959f095893c7ec1c8e35a0e33820
CHRISTIAN MOURA DOS SANTOS <cmourasiga@gmail.com>
2025-05-20 21:41:06 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -240,3 +240,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
+reativarContatosPendentes();
+verificarEventosParaLembrete();
